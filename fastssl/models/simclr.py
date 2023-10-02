928d85406f56816fe231fcf1d0b055bb509cf3aa
Matteo Gamba <argon@bruinen>
2023-10-02 16:33:59 +0200

    Alternative NT_XENT implementation.

--- fastssl/models/simclr.py
+++ fastssl/models/simclr.py
@@ -8,37 +8,28 @@ import torch.nn.functional as F
 from fastssl.models.ssl import SSL
 from fastssl.models.backbone import BackBone
 
-
 def SimCLRLoss(model, inp, _temperature=0.05):
+    """ Perform model forward pass and compute the SimCLR loss,
+        a.k.a. the normalized temperature-scaled cross entropy.
+        
+        Args:
+            model: a torch.nn.Module
+            inp: a torch.Tensor (or list thereof)
+        Returns:
+            loss: scalar tensor
     """
-    Peform model forward pass and compute the BYOL loss.
-    Args:
-        model: a torch.nn.Module
-        inp: a torch.Tensor
-    Returns:
-        loss: scalar tensor
-    """
-
     # generate samples from tuple
     inp = list(inp)
     _ = inp.pop(1)
     num_augs = len(inp)
     for x in inp:
         x = x.cuda(non_blocking=True)
-    # (x1, x2), _ = inp
-    # x1, x2 = x1.cuda(non_blocking=True), x2.cuda(non_blocking=True)
 
     bsz = inp[0].shape[0]
-    # bsz = x1.shape[0]
 
     # forward pass
     z_list = [model(x) for x in inp]
-    # z1 = model(x1)   #NXD
-    # z2 = model(x2)   #NXD
-
     z_norm_list = [(z - z.mean(0)) / z.std(0) for z in z_list]
-    # z1_norm = F.normalize(z1, dim=-1)
-    # z2_norm = F.normalize(z2, dim=-1)
 
     loss = 0.0
     # compute sum across all patches of each image for each embedding dim
@@ -48,29 +39,101 @@ def SimCLRLoss(model, inp, _temperature=0.05):
         z1_norm = z_norm_list[i]
         # take mean embedding of all other patches
         z2_norm = (z_norm_sum - z_norm_list[i]) / (num_augs - 1)
-        all_z_norm = torch.cat([z1_norm, z2_norm], dim=0)
 
-        similarity_scores = (all_z_norm @ all_z_norm.T) / _temperature
-        eps = 1e-9
+        z1_norm_large = z1_norm
+        z2_norm_large = z2_norm
+        
+        labels = F.one_hot(torch.arange(bsz), bsz*2)
+        masks = F.one_hot(torch.arange(bsz), bsz)
+        
+        logits_aa = torch.matmul(z1_norm, z1_norm_large.T) / _temperature
+        logits_aa = logits_aa - masks
 
-        ones = torch.ones(bsz)
-        mask = (torch.diag(ones, bsz) + torch.diag(ones, -bsz)).cuda(non_blocking=True)
-
-        # subtract max value for stability
-        logits = (
-            similarity_scores - similarity_scores.max(dim=-1, keepdim=True)[0].detach()
+        logits_bb = torch.matmul(z2_norm, z2_norm_large.T) / _temperature
+        logits_bb = logits_bb - masks
+        logits_ab = torch.matmul(z1_norm, z2_norm_large.T) / _temperature
+        logits_ba = torch.matmul(z2_norm, z1_norm_large.T) / _temperature
+        
+        softmax_a = F.softmax(
+            torch.cat((logits_ab, logits_aa), 1)
         )
-        # remove the diagonal entries of all 1./_temperature because they are cosine
-        # similarity of one image to itself.
-        exp_logits = torch.exp(logits) * (1 - torch.eye(2 * bsz)).cuda(
-            non_blocking=True
+        softmax_b = F.softmax(
+            torch.cat((logits_ba, logits_bb), 1)
         )
+        loss_a = F.cross_entropy(
+            softmax_a, labels
+        )
+        loss_b = F.cross_entropy(
+            softmax_b, labels
+        )
+        loss += loss_a + loss_b
 
-        log_likelihood = -logits + torch.log(exp_logits.sum(dim=-1, keepdim=True) + eps)
-
-        loss += (log_likelihood * mask).sum() / mask.sum()
     loss = loss / num_augs
     return loss
+
+
+#def SimCLRLoss(model, inp, _temperature=0.05):
+#    """
+#    Peform model forward pass and compute the BYOL loss.
+#    Args:
+#        model: a torch.nn.Module
+#        inp: a torch.Tensor
+#    Returns:
+#        loss: scalar tensor
+#    """
+
+#    # generate samples from tuple
+#    inp = list(inp)
+#    _ = inp.pop(1)
+#    num_augs = len(inp)
+#    for x in inp:
+#        x = x.cuda(non_blocking=True)
+#    # (x1, x2), _ = inp
+#    # x1, x2 = x1.cuda(non_blocking=True), x2.cuda(non_blocking=True)
+
+#    bsz = inp[0].shape[0]
+#    # bsz = x1.shape[0]
+
+#    # forward pass
+#    z_list = [model(x) for x in inp]
+#    # z1 = model(x1)   #NXD
+#    # z2 = model(x2)   #NXD
+
+#    z_norm_list = [(z - z.mean(0)) / z.std(0) for z in z_list]
+#    # z1_norm = F.normalize(z1, dim=-1)
+#    # z2_norm = F.normalize(z2, dim=-1)
+
+#    loss = 0.0
+#    # compute sum across all patches of each image for each embedding dim
+#    z_norm_sum = torch.sum(torch.stack(z_norm_list), dim=0)
+#    for i in range(num_augs):
+#        # take embedding of one patch
+#        z1_norm = z_norm_list[i]
+#        # take mean embedding of all other patches
+#        z2_norm = (z_norm_sum - z_norm_list[i]) / (num_augs - 1)
+#        all_z_norm = torch.cat([z1_norm, z2_norm], dim=0)
+
+#        similarity_scores = (all_z_norm @ all_z_norm.T) / _temperature
+#        eps = 1e-9
+
+#        ones = torch.ones(bsz)
+#        mask = (torch.diag(ones, bsz) + torch.diag(ones, -bsz)).cuda(non_blocking=True)
+
+#        # subtract max value for stability
+#        logits = (
+#            similarity_scores - similarity_scores.max(dim=-1, keepdim=True)[0].detach()
+#        )
+#        # remove the diagonal entries of all 1./_temperature because they are cosine
+#        # similarity of one image to itself.
+#        exp_logits = torch.exp(logits) * (1 - torch.eye(2 * bsz)).cuda(
+#            non_blocking=True
+#        )
+
+#        log_likelihood = -logits + torch.log(exp_logits.sum(dim=-1, keepdim=True) + eps)
+
+#        loss += (log_likelihood * mask).sum() / mask.sum()
+#    loss = loss / num_augs
+#    return loss
 
 
 class SimCLR(SSL):
