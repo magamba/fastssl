2d0dfb9bc909818e8b196e26451ee968dcf6d97c
Matteo Gamba <x_matga@berzelius001.eth.cluster>
2023-10-06 00:05:47 +0200

    Several bugfixes.

--- aggregate_data.py
+++ aggregate_data.py
@@ -6,8 +6,6 @@ import os
 import sys
 import logging
 
-from core.utils import init_logging
-
 plots_path = "./plots"
 name = 'ssl'
 
@@ -26,20 +24,24 @@ except KeyError:
 
 def parse_stats(fname, stats, metric, epoch):
     try:
-        metric_vals = stats[metric]
-    except ValueError:
+        metric_vals = stats[str(metric)]
+    except KeyError:
         logger.info(f"Metric {metric} not found in {fname}")
         missing_runs.append(fname)
-        
-    keys = [ i[0] for i in metric_vals ]
-    vals = [ i[1] for i in metric_vals ]
-    try:
-        key = keys.index(epoch)
-        val = vals[key]
-    except ValueError:
-        logger.info(f"Epoch {epoch} missing for metric {metric} in {fname}")
-        missing_runs.append(fname)
-        val = 0
+        return 0
+
+    if metric not in figure1_conf["performance_metrics"]:
+        keys = [ i[0] for i in metric_vals ]
+        vals = [ i[1] for i in metric_vals ]
+        try:
+            key = keys.index(epoch)
+            val = vals[key]
+        except ValueError:
+            logger.info(f"Epoch {epoch} missing for metric {metric} in {fname}")
+            missing_runs.append(fname)
+            val = 0
+    else:
+        val = metric_vals[-1]
     return val
 
 
@@ -63,7 +65,7 @@ figure1_conf = {
     "algorithms": ["barlow_twins", "byol"],
     "base_models": ["resnet18"],
     "widths": list(range(1,65)),
-    "seeds" : [1, 2, 3],
+    "seeds" : [0, 1, 2],
     "epochs": {
         "barlow_twins": 100,
         "byol": 300,
@@ -96,20 +98,21 @@ figure1_conf.update({
             dataset: {
                 0: {
                     width: [ f"{root_dir}_barlow_twins-{dataset}/resnet18/width{width}/2_augs/lambd_0.005000_pdim_{32 * width}_lr_0.001_wd_1e-06/1_augs_eval/results_{dataset}_alpha_linear_200_seed_{seed}.npy"  for seed in figure1_conf["seeds"] ] for width in figure1_conf["widths"]
-                }, 
-                noise: {
+                },
+                **{noise: {
                     width: [ f"{root_dir}_barlow_twins_noise{noise}-{dataset}/resnet18/width{width}/2_augs/lambd_0.005000_pdim_{32 * width}_lr_0.001_wd_1e-06/1_augs_eval/results_{dataset}_alpha_linear_200_seed_{seed}.npy"  for seed in figure1_conf["seeds"] ] for width in figure1_conf["widths"]
-                } for noise in figure1_conf["noise_configs"]
+                } for noise in figure1_conf["noise_configs"][1:]}
             } for dataset in figure1_conf["datasets"]
         },
         "byol": {
             dataset: {
                 0 : {
                     width: [ f"{root_dir}_byol-{dataset}/resnet18/width{width}/2_augs/lambd_0.007812_pdim_{width * 32}_lr_0.001_wd_1e-06/1_augs_eval/results_{dataset}_alpha_linear_200_seed_{seed}.npy"  for seed in figure1_conf["seeds"] ] for width in figure1_conf["widths"]
-                }, 
-                noise: {
-                    width: [ f"{root_dir}_byol_noise{noise}-{dataset}/resnet18/width{width}/2_augs/lambd_0.007812_pdim_{width * 32}_lr_0.001_wd_1e-06//1_augs_eval/results_{dataset}_alpha_linear_200_seed_{seed}.npy"  for seed in figure1_conf["seeds"] ] for width in figure1_conf["widths"]
-                } for noise in figure1_conf["noise_configs"]
+                },
+                **{noise: {
+                    width: [ f"{root_dir}_byol_noise{noise}-{dataset}/resnet18/width{width}/2_augs/lambd_0.007812_pdim_{width * 32}_lr_0.001_wd_1e-06/1_augs_eval/results_{dataset}_alpha_linear_200_seed_{seed}.npy"  for seed in figure1_conf["seeds"] ] for width in figure1_conf["widths"]
+                } for noise in figure1_conf["noise_configs"][1:]}
+            } for dataset in figure1_conf["datasets"]
         },
     },
 })
@@ -123,35 +126,36 @@ def aggregate_data(destdir=plots_path):
     nseeds = len(figure1_conf["seeds"])
     nmetrics = len(figure1_conf["metrics"])
     nwidths = len(figure1_conf["widths"])
-    
+    nnoise = len(figure1_conf["noise_configs"])
     figure1_data = figure1_conf
 
     plot_data = {}
     for algorithm in figure1_conf["algorithms"]:
         plot_data[algorithm] = {}
-        epoch = plot_data["epochs"][algorithm]
         for dataset in figure1_conf["datasets"]:
             plot_data[algorithm][dataset] = {}
             for base_model in figure1_conf["base_models"]:
                 plot_data[algorithm][dataset][base_model] = np.zeros((nwidths, nmetrics, nseeds))
                 for w_id, width in enumerate(figure1_conf["widths"]):
                     for s_id in range(nseeds):
-                        fname = plot_data["filenames"][algorithm][dataset][width][seed]
+                        fname = figure1_conf["filenames"][algorithm][dataset][width][s_id]
                         logger.info(f"Loading {fname}")
                         try:
                             stats = np.load(
                                 fname,
                                 allow_pickle=True
-                            )
+                            ).tolist()
                             for m_id, metric in enumerate(figure1_conf["metrics"]):
+                                epoch = figure1_conf["epochs"][algorithm]
+                                logger.info(f"Parsing {base_model}_{width} epoch {epoch} {metric} seed {s_id}")
                                 plot_data[algorithm][dataset][base_model][w_id, m_id, s_id] = parse_stats(fname, stats, metric, epoch)
                                 
                         except FileNotFoundError:
                             logger.info(f"File not found: {fname}")
                             missing_runs.append(fname)
                             
-            plot_data[algorithm][dataset][base_model] = \
-                plot_data[algorithm][dataset][base_model].tolist()
+                plot_data[algorithm][dataset][base_model] = \
+                    plot_data[algorithm][dataset][base_model].tolist()
         
     figure1_data.pop("filenames")
     figure1_data["plot_data"] = plot_data
@@ -160,29 +164,31 @@ def aggregate_data(destdir=plots_path):
     performance_data = {}
     for algorithm in figure1_conf["algorithms"]:
         plot_data[algorithm] = {}
-        epoch = plot_data["epochs"]["linear"]
+        epoch = figure1_conf["epochs"]["linear"]
         for dataset in figure1_conf["datasets"]:
             plot_data[algorithm][dataset] = {}
             for base_model in figure1_conf["base_models"]:
-                plot_data[algorithm][dataset][base_model] = np.zeros((nwidths, nmetrics, nseeds))
-                for w_id, width in enumerate(figure1_conf["widths"]):
-                    for s_id in range(nseeds):
-                        fname = plot_data["performance_filenames"][algorithm][dataset][width][seed]
-                        logger.info(f"Loading {fname}")
-                        try:
-                            stats = np.load(
-                                fname,
-                                allow_pickle=True
-                            )
-                            for m_id, metric in enumerate(figure1_conf["metrics"]):
-                                plot_data[algorithm][dataset][base_model][w_id, m_id, s_id] = parse_stats(fname, stats, metric, epoch)
+                plot_data[algorithm][dataset][base_model] = np.zeros((nnoise, nwidths, nmetrics, nseeds))
+                for n_id, noise in enumerate(figure1_conf["noise_configs"]):
+                    for w_id, width in enumerate(figure1_conf["widths"]):
+                        for s_id in range(nseeds):
+                            fname = figure1_conf["performance_filenames"][algorithm][dataset][noise][width][s_id]
+                            logger.info(f"Loading {fname}")
+                            try:
+                                stats = np.load(
+                                    fname,
+                                    allow_pickle=True
+                                ).tolist()
+                                for m_id, metric in enumerate(figure1_conf["performance_metrics"]):
+                                    logger.info(f"Parsing {base_model}_{width} epoch {epoch} {metric} seed {s_id}")
+                                    plot_data[algorithm][dataset][base_model][n_id, w_id, m_id, s_id] = parse_stats(fname, stats, metric, epoch)
                                 
-                        except FileNotFoundError:
-                            logger.info(f"File not found: {fname}")
-                            missing_runs.append(fname)
+                            except FileNotFoundError:
+                                logger.info(f"File not found: {fname}")
+                                missing_runs.append(fname)
                             
-            plot_data[algorithm][dataset][base_model] = \
-                plot_data[algorithm][dataset][base_model].tolist()
+                plot_data[algorithm][dataset][base_model] = \
+                    plot_data[algorithm][dataset][base_model].tolist()
     
     figure1_data.pop("performance_filenames")
     figure1_data["performance_data"] = performance_data
@@ -208,9 +214,28 @@ def aggregate_stats(fig_id):
         destdir = os.path.join(plots_path, 'figure1')
         if not os.path.exists(destdir):
             os.makedirs(destdir)
-        aggregate_fig1(destdir)
+        aggregate_data(destdir)
     else:
         raise ValueError(f"Invalid figure id {fig_id}")
+
+
+def init_logging(logger_name, logfile, log_level: str):
+    numeric_level = getattr(logging, log_level.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError("Invalid log level: %s" % log_level)
+
+    formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+
+    f_handler = logging.FileHandler(logfile)
+    f_handler.setLevel(numeric_level)
+    c_handler = logging.StreamHandler()
+    c_handler.setLevel(numeric_level)
+    f_handler.setFormatter(formatter)
+    c_handler.setFormatter(formatter)
+    
+    logging.basicConfig(level=numeric_level, handlers=[f_handler, c_handler])
+    logger = logging.getLogger(logger_name)
+    return logger
 
 
 def main():
@@ -219,7 +244,7 @@ def main():
         os.makedirs(plots_path)
 
     logfile = os.path.join(plots_path, 'aggregate_data.log')
-    init_logging(None, logfile, 'info', None)
+    init_logging(None, logfile, 'info')
     
     global logger
     logger = logging.getLogger()
