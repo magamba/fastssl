220649e29c01a4bd3cd620e5535350caecc4171e
Matteo Gamba <argon@bruinen>
2024-05-14 14:09:27 +0200

    Fixed indentation error.

--- write_ffcv_datasets.py
+++ write_ffcv_datasets.py
@@ -106,7 +106,7 @@ def subsample_dataset(dataset, classes_to_keep, samples_per_class, train=False):
     """
     class_idx = { c: dataset.class_to_idx[c] for c in classes_to_keep }
     targets = np.asarray(dataset.targets)
-    
+
     # select samples to keep according to classes_to_keep and samples_per_class
     mask_per_class = [ targets == class_idx[c] for c in class_idx ]
     samples_mask = np.zeros_like(mask_per_class[0])
@@ -116,107 +116,107 @@ def subsample_dataset(dataset, classes_to_keep, samples_per_class, train=False):
             mask[cut_idx:] = False
         mask_per_class[i] = mask
         samples_mask = np.logical_or(samples_mask, mask)
-  
+
     sample_idx = np.where(samples_mask)[0]
-    
+
     targets = targets[sample_idx]
     dataset.data = dataset.data[sample_idx]
     dataset.classes = classes_to_keep
-    
+
     for cid, c in enumerate(class_idx):
         targets[targets == class_idx[c]] = cid
         class_idx[c] = cid
-    
+
     dataset.targets = targets
     dataset.class_to_idx = class_idx
-    
+
     return dataset
 
 def add_label_noise(targets,noise_percentage=0.1,seed=1234):
-	from numpy.random import default_rng
-	rng = default_rng(seed)
+    from numpy.random import default_rng
+    rng = default_rng(seed)
 
-	if not isinstance(targets,torch.Tensor):
-		targets_tensor = torch.LongTensor(targets)
-	else:
-		targets_tensor = torch.clone(targets)
+    if not isinstance(targets,torch.Tensor):
+        targets_tensor = torch.LongTensor(targets)
+    else:
+        targets_tensor = torch.clone(targets)
 
-	targets_shape = targets_tensor.shape
-	corrupt_idx = int(np.ceil(targets_tensor.shape[0] * noise_percentage))
-	num_classes = targets_tensor.unique().shape[0]
-	if corrupt_idx == 0:
-		return targets
+    targets_shape = targets_tensor.shape
+    corrupt_idx = int(np.ceil(targets_tensor.shape[0] * noise_percentage))
+    num_classes = targets_tensor.unique().shape[0]
+    if corrupt_idx == 0:
+        return targets
 
-	noise = torch.zeros_like(targets_tensor)
-	noise[0:corrupt_idx] = torch.from_numpy(
-		rng.integers(low=1, high=num_classes, size=(corrupt_idx,))
-	)
-	shuffle = torch.from_numpy(rng.permutation(noise.shape[0]))
-	noise = noise[shuffle]
+    noise = torch.zeros_like(targets_tensor)
+    noise[0:corrupt_idx] = torch.from_numpy(
+        rng.integers(low=1, high=num_classes, size=(corrupt_idx,))
+    )
+    shuffle = torch.from_numpy(rng.permutation(noise.shape[0]))
+    noise = noise[shuffle]
 
-	new_labels = (
-		(targets_tensor + noise) % num_classes
-		).type(torch.LongTensor)
-	
-	num_targets_match = (new_labels==targets_tensor).sum()
-	print("Amount of label noise added: {:.3f}".format(
-		100.0*(1-num_targets_match/targets_shape[0])))
-	
-	if not isinstance(targets,torch.Tensor):
-		# assuming targets was a list
-		new_labels = list(new_labels)
-	return new_labels
+    new_labels = (
+        (targets_tensor + noise) % num_classes
+        ).type(torch.LongTensor)
+
+    num_targets_match = (new_labels==targets_tensor).sum()
+    print("Amount of label noise added: {:.3f}".format(
+        100.0*(1-num_targets_match/targets_shape[0])))
+
+    if not isinstance(targets,torch.Tensor):
+        # assuming targets was a list
+        new_labels = list(new_labels)
+    return new_labels
 
 if dataset=='cifar10':
-	CIFAR10 = torchvision.datasets.CIFAR10
-	
-	if noise_level > 0:
-		CIFAR10 = with_indices(
-			CIFAR10
-		)
+    CIFAR10 = torchvision.datasets.CIFAR10
 
-	trainset = CIFAR10(
-	    root=dataset_folder, train=True, download=False, transform=None
-	)
-	    
-	testset = torchvision.datasets.CIFAR10(
-	    root=dataset_folder, train=False, download=False, transform=None
-	)
-
-if dataset=='cifar100':
-	CIFAR100 = torchvision.datasets.CIFAR100
-	if noise_level > 0:
-		CIFAR100 = with_indices(
-			CIFAR100
-		)
-
-	trainset = CIFAR100(
-	    root=dataset_folder, train=True, download=False, transform=None
-	)
-	testset = torchvision.datasets.CIFAR100(
-	    root=dataset_folder, train=False, download=False, transform=None
-	)
-
-if dataset=='cifar10c':
-        trainset = None
-        testset = CIFAR10C(
-            root=dataset_folder, noise_type=noise_type, train=False, download=False, transform=None
+    if noise_level > 0:
+        CIFAR10 = with_indices(
+            CIFAR10
         )
 
+    trainset = CIFAR10(
+        root=dataset_folder, train=True, download=False, transform=None
+    )
+        
+    testset = torchvision.datasets.CIFAR10(
+        root=dataset_folder, train=False, download=False, transform=None
+    )
+
+if dataset=='cifar100':
+    CIFAR100 = torchvision.datasets.CIFAR100
+    if noise_level > 0:
+        CIFAR100 = with_indices(
+            CIFAR100
+        )
+
+    trainset = CIFAR100(
+        root=dataset_folder, train=True, download=False, transform=None
+    )
+    testset = torchvision.datasets.CIFAR100(
+        root=dataset_folder, train=False, download=False, transform=None
+    )
+
+if dataset=='cifar10c':
+    trainset = None
+    testset = CIFAR10C(
+        root=dataset_folder, noise_type=noise_type, train=False, download=False, transform=None
+    )
+
 elif dataset=='stl10':
-        STL10 = torchvision.datasets.STL10
-        if noise_level > 0:
-                STL10 = with_indices(
-                    STL10
-                )
+    STL10 = torchvision.datasets.STL10
+    if noise_level > 0:
+        STL10 = with_indices(
+            STL10
+        )
 
     unlabeledset = torchvision.datasets.STL10(
         root=dataset_folder, split="unlabeled", download=False, transform=None
     )
-        trainset = STL10(
-            root=dataset_folder, split='train', download=False, transform=None)
-        testset = torchvision.datasets.STL10(
-            root=dataset_folder, split='test', download=False, transform=None)
+    trainset = STL10(
+        root=dataset_folder, split='train', download=False, transform=None)
+    testset = torchvision.datasets.STL10(
+        root=dataset_folder, split='test', download=False, transform=None)
 
 dataset_str = f"{dataset}_" if noise_level == 0 and not subsample_classes and dataset != "cifar10c" else ""
 train_beton_fpath = os.path.join(ffcv_folder, dataset_str + "train.beton")
@@ -224,49 +224,49 @@ test_beton_fpath = os.path.join(ffcv_folder, dataset_str + "test.beton")
 	
 ## WRITE TO BETON FILES
 if write_dataset:
-	datasets = {'train': trainset, 'test':testset}
-	for name,ds in datasets.items():
-                if dataset == "cifar10c" and name == "train": continue
-		#breakpoint()
-		if dataset == "cifar10" and subsample_classes:
-		    ds = subsample_dataset(ds, classes_to_keep, samples_per_class, train= name == "train")
-		
-		if name == 'train' and noise_level>0:
-			try:
-				targets = ds.targets
-			except AttributeError:
-				targets = ds.labels
-			targets_orig = targets.copy()
-			new_targets = add_label_noise(targets=targets,
-				 noise_percentage=noise_level/100.0)
-			try:
-				_ = ds.targets
-				ds.targets = new_targets
-			except AttributeError:
-				ds.labels = new_targets
-				
-			ds._targets_orig = targets_orig
+    datasets = {'train': trainset, 'test':testset}
+    for name,ds in datasets.items():
+        if dataset == "cifar10c" and name == "train": continue
+        #breakpoint()
+        if dataset == "cifar10" and subsample_classes:
+            ds = subsample_dataset(ds, classes_to_keep, samples_per_class, train= name == "train")
+        
+        if name == 'train' and noise_level>0:
+            try:
+                targets = ds.targets
+            except AttributeError:
+                targets = ds.labels
+            targets_orig = targets.copy()
+            new_targets = add_label_noise(targets=targets,
+                noise_percentage=noise_level/100.0)
+            try:
+                _ = ds.targets
+                ds.targets = new_targets
+            except AttributeError:
+                ds.labels = new_targets
+	            
+            ds._targets_orig = targets_orig
 
-		path = train_beton_fpath if name=='train' else test_beton_fpath
-		fields = {
-			'image': RGBImageField(),
-			'label': IntField(),
-		}
-		if noise_level > 0 and name == "train":
-			fields.update({
-				'ground_truth': IntField(),
-				'sample_idx': IntField(),
-			})
-		writer = DatasetWriter(path, fields)
-		writer.from_indexed_dataset(ds)
-	if dataset=='stl10':
-            datasets = {"unlabeled": unlabeledset}
-            unlabeled_beton_fpath = os.path.join(ffcv_folder, "unlabeled.beton")
-            for name, ds in datasets.items():
-                breakpoint()
-                path = unlabeled_beton_fpath
-                writer = DatasetWriter(path, {"image": RGBImageField(), "label": IntField()})
-                writer.from_indexed_dataset(ds)
+        path = train_beton_fpath if name=='train' else test_beton_fpath
+        fields = {
+            'image': RGBImageField(),
+            'label': IntField(),
+        }
+        if noise_level > 0 and name == "train":
+            fields.update({
+                'ground_truth': IntField(),
+                'sample_idx': IntField(),
+            })
+        writer = DatasetWriter(path, fields)
+        writer.from_indexed_dataset(ds)
+    if dataset=='stl10':
+        datasets = {"unlabeled": unlabeledset}
+        unlabeled_beton_fpath = os.path.join(ffcv_folder, "unlabeled.beton")
+        for name, ds in datasets.items():
+            breakpoint()
+            path = unlabeled_beton_fpath
+            writer = DatasetWriter(path, {"image": RGBImageField(), "label": IntField()})
+            writer.from_indexed_dataset(ds)
 
 ## VERIFY the WRITTEN DATASET
 BATCH_SIZE = 500
@@ -289,10 +289,10 @@ for name in ["train", "test"]:
             Convert(torch.float32),
         ]
     )
-    
+
     pipelines = {'image': image_pipeline, 'label': label_pipeline}
-	if noise_level > 0:
-		pipelines.update({'ground_truth': label_pipeline, 'sample_idx': label_pipeline})
+    if noise_level > 0:
+        pipelines.update({'ground_truth': label_pipeline, 'sample_idx': label_pipeline})
 
     loaders[name] = Loader(
         os.path.join(ffcv_folder, "{}.beton".format(name)),
@@ -308,80 +308,80 @@ transform_test = torchvision.transforms.Compose([torchvision.transforms.ToTensor
 
 skip_train = False
 if dataset=='cifar10':
-	dataset_cls = torchvision.datasets.CIFAR10
-	if noise_level > 0:
-		dataset_cls = with_indices(dataset_cls)
-	trainset = dataset_cls(
-	    root=dataset_folder, train=True, download=False, transform=transform_test)
-	testset = torchvision.datasets.CIFAR10(
-	    root=dataset_folder, train=False, download=False, transform=transform_test)
-	if subsample_classes:
-            trainset = subsample_dataset(trainset, classes_to_keep, samples_per_class, train=True)
-            testset = subsample_dataset(testset, classes_to_keep, samples_per_class)
+    dataset_cls = torchvision.datasets.CIFAR10
+    if noise_level > 0:
+        dataset_cls = with_indices(dataset_cls)
+    trainset = dataset_cls(
+        root=dataset_folder, train=True, download=False, transform=transform_test)
+    testset = torchvision.datasets.CIFAR10(
+        root=dataset_folder, train=False, download=False, transform=transform_test)
+    if subsample_classes:
+        trainset = subsample_dataset(trainset, classes_to_keep, samples_per_class, train=True)
+        testset = subsample_dataset(testset, classes_to_keep, samples_per_class)
 elif dataset=='cifar100':
-	dataset_cls = torchvision.datasets.CIFAR100
-	if noise_level > 0:
-		dataset_cls = with_indices(dataset_cls)
-	trainset = dataset_cls(
-	    root=dataset_folder, train=True, download=False, transform=transform_test)
-	testset = torchvision.datasets.CIFAR100(
-	    root=dataset_folder, train=False, download=False, transform=transform_test)
+    dataset_cls = torchvision.datasets.CIFAR100
+    if noise_level > 0:
+        dataset_cls = with_indices(dataset_cls)
+    trainset = dataset_cls(
+        root=dataset_folder, train=True, download=False, transform=transform_test)
+    testset = torchvision.datasets.CIFAR100(
+        root=dataset_folder, train=False, download=False, transform=transform_test)
 elif dataset=='cifar10c':
-        skip_train = True
-	testset = CIFAR10C(
-	    root=dataset_folder, noise_type=noise_type, train=False, download=False, transform=None)
+    skip_train = True
+    testset = CIFAR10C(
+    root=dataset_folder, noise_type=noise_type, train=False, download=False, transform=None)
 elif dataset=='stl10':
-	dataset_cls = torchvision.datasets.STL10
-	if noise_level > 0:
-		dataset_cls = with_indices(dataset_cls)
-	trainset = dataset_cls(
-	    root=dataset_folder, split='train', download=False, transform=transform_test)
-	testset = torchvision.datasets.STL10(
-	    root=dataset_folder, split='test', download=False, transform=transform_test)
+    dataset_cls = torchvision.datasets.STL10
+    if noise_level > 0:
+        dataset_cls = with_indices(dataset_cls)
+    trainset = dataset_cls(
+        root=dataset_folder, split='train', download=False, transform=transform_test)
+    testset = torchvision.datasets.STL10(
+        root=dataset_folder, split='test', download=False, transform=transform_test)
 
 if not skip_train:
-	trainloader = torch.utils.data.DataLoader(
-		trainset, batch_size=BATCH_SIZE, shuffle=False, num_workers=1)
-testloader = torch.utils.data.DataLoader(
-	testset, batch_size=BATCH_SIZE, shuffle=False, num_workers=1)
+    trainloader = torch.utils.data.DataLoader(
+	    trainset, batch_size=BATCH_SIZE, shuffle=False, num_workers=1)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=BATCH_SIZE, shuffle=False, num_workers=1)
 
 if not skip_train:
-	if noise_level > 0:
-		X_ffcv, y_ffcv, gt_ffcv, sid_ffcv = next(iter(loaders['train']))
-		X_tv, y_tv, gt_tv, sid_tv = next(iter(trainloader))
-	else:
-		X_ffcv, y_ffcv = next(iter(loaders['train']))
-		X_tv, y_tv = next(iter(trainloader))
-	print('FFCV stats:',X_ffcv.shape,X_ffcv.mean(),X_ffcv.min(),X_ffcv.max())
-	print('torchV stats:',X_tv.shape,X_tv.mean(),X_tv.min(),X_tv.max())
-	print(torch.allclose(X_ffcv,X_tv*255.))
-	#breakpoint()
+    if noise_level > 0:
+        X_ffcv, y_ffcv, gt_ffcv, sid_ffcv = next(iter(loaders['train']))
+        X_tv, y_tv, gt_tv, sid_tv = next(iter(trainloader))
+    else:
+        X_ffcv, y_ffcv = next(iter(loaders['train']))
+        X_tv, y_tv = next(iter(trainloader))
+    print('FFCV stats:',X_ffcv.shape,X_ffcv.mean(),X_ffcv.min(),X_ffcv.max())
+    print('torchV stats:',X_tv.shape,X_tv.mean(),X_tv.min(),X_tv.max())
+    print(torch.allclose(X_ffcv,X_tv*255.))
+    #breakpoint()
 
 	# calculate mean and std of dataset
-	print("ffcv dataset stats...")
-	mean = 0.0
-	std = 0.0
-	nb_samples = 0.
-	for inp in loaders['train']:
-		img = inp[0]
-		batch_samples = img.size(0)
-		data = img.view(batch_samples,img.size(1),-1)
-		mean+= data.mean(2).sum(0)
-		std+= data.std(2).sum(0)
-		nb_samples += batch_samples
-	mean /= nb_samples
-	std /= nb_samples
-	print("Train Dataset mean",mean)
-	print("Train Dataset std",std)
+    print("ffcv dataset stats...")
+    mean = 0.0
+    std = 0.0
+    nb_samples = 0.
+    for inp in loaders['train']:
+        img = inp[0]
+        batch_samples = img.size(0)
+        data = img.view(batch_samples,img.size(1),-1)
+        mean+= data.mean(2).sum(0)
+        std+= data.std(2).sum(0)
+        nb_samples += batch_samples
+    mean /= nb_samples
+    std /= nb_samples
+    print("Train Dataset mean",mean)
+    print("Train Dataset std",std)
 mean = 0.0
 std = 0.0
 nb_samples = 0.
 for img,_ in loaders['test']:
-	batch_samples = img.size(0)
-	data = img.view(batch_samples,img.size(1),-1)
-	mean+= data.mean(2).sum(0)
-	std+= data.std(2).sum(0)
-	nb_samples += batch_samples
+    batch_samples = img.size(0)
+    data = img.view(batch_samples,img.size(1),-1)
+    mean+= data.mean(2).sum(0)
+    std+= data.std(2).sum(0)
+    nb_samples += batch_samples
 mean /= nb_samples
 std /= nb_samples
 print("Test Dataset mean",mean)
@@ -389,32 +389,32 @@ print("Test Dataset std",std)
 
 print("tv dataset stats...")
 if not skip_train:
-	mean = 0.0
-	std = 0.0
-	nb_samples = 0.
-	for inp in trainloader:
-		img = inp[0]
-		batch_samples = img.size(0)
-		data = img.view(batch_samples,img.size(1),-1)
-		mean+= data.mean(2).sum(0)
-		std+= data.std(2).sum(0)
-		nb_samples += batch_samples
-	mean /= nb_samples
-	std /= nb_samples
-	print("Train Dataset mean", mean)
-	print("Train Dataset std", std)
-	print("Train Dataset mean*255", mean*255)
-	print("Train Dataset std*255", std*255)
+    mean = 0.0
+    std = 0.0
+    nb_samples = 0.
+    for inp in trainloader:
+        img = inp[0]
+        batch_samples = img.size(0)
+        data = img.view(batch_samples,img.size(1),-1)
+        mean+= data.mean(2).sum(0)
+        std+= data.std(2).sum(0)
+        nb_samples += batch_samples
+    mean /= nb_samples
+    std /= nb_samples
+    print("Train Dataset mean", mean)
+    print("Train Dataset std", std)
+    print("Train Dataset mean*255", mean*255)
+    print("Train Dataset std*255", std*255)
 
 mean = 0.0
 std = 0.0
 nb_samples = 0.
 for img,_ in testloader:
-	batch_samples = img.size(0)
-	data = img.view(batch_samples,img.size(1),-1)
-	mean+= data.mean(2).sum(0)
-	std+= data.std(2).sum(0)
-	nb_samples += batch_samples
+    batch_samples = img.size(0)
+    data = img.view(batch_samples,img.size(1),-1)
+    mean+= data.mean(2).sum(0)
+    std+= data.std(2).sum(0)
+    nb_samples += batch_samples
 mean /= nb_samples
 std /= nb_samples
 print("Test Dataset mean",mean)
@@ -423,22 +423,22 @@ print("Test Dataset mean*255", mean*255)
 print("Test Dataset std*255", std*255)
 
 if noise_level > 0:
-	num_corrupted_ffcv = 0.
-	num_corrupted = 0.
-	num_samples = 0
-	for (_, t_ffcv, gt_ffcv, sid_ffcv), (_, t_tv, gt_tv, sid_tv) in zip(loaders["train"], trainloader):
-		assert torch.all(torch.eq(sid_ffcv, sid_tv))
-		assert torch.all(torch.eq(gt_ffcv, gt_tv))
-		assert torch.all(torch.eq(gt_tv, t_tv))
-		num_corrupted_ffcv += torch.sum(torch.ne(t_ffcv, gt_ffcv)).float().item()
-		num_corrupted += torch.sum(torch.ne(t_ffcv, t_tv)).float().item()
-		num_samples += t_ffcv.shape[0]
-	num_corrupted_ffcv = int(num_corrupted_ffcv / num_samples *100)
-	num_corrupted = int(num_corrupted / num_samples * 100)
-	print(f"FFCV - ratio of corrupted samples: {num_corrupted_ffcv}%")
-	print(f"FFCV vs TV - ratio of corrupted samples: {num_corrupted}%")
-	assert (num_corrupted == num_corrupted_ffcv)
-	assert (num_corrupted == int(noise_level))
+    num_corrupted_ffcv = 0.
+    num_corrupted = 0.
+    num_samples = 0
+    for (_, t_ffcv, gt_ffcv, sid_ffcv), (_, t_tv, gt_tv, sid_tv) in zip(loaders["train"], trainloader):
+        assert torch.all(torch.eq(sid_ffcv, sid_tv))
+        assert torch.all(torch.eq(gt_ffcv, gt_tv))
+        assert torch.all(torch.eq(gt_tv, t_tv))
+        num_corrupted_ffcv += torch.sum(torch.ne(t_ffcv, gt_ffcv)).float().item()
+        num_corrupted += torch.sum(torch.ne(t_ffcv, t_tv)).float().item()
+        num_samples += t_ffcv.shape[0]
+    num_corrupted_ffcv = int(num_corrupted_ffcv / num_samples *100)
+    num_corrupted = int(num_corrupted / num_samples * 100)
+    print(f"FFCV - ratio of corrupted samples: {num_corrupted_ffcv}%")
+    print(f"FFCV vs TV - ratio of corrupted samples: {num_corrupted}%")
+    assert (num_corrupted == num_corrupted_ffcv)
+    assert (num_corrupted == int(noise_level))
 
 ### ===============================================================================
 # Usage:
