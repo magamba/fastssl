1. Fixed eval transforms for ImageNet; 2. Label corruption algorithms for imagenet. 3. Low-memory covariance computation. 4. Added wandb information to setup script.

fastssl/data/imagenet_dataloaders.py
@@ -1,4 +1,4 @@
-# Ffcv dataloaders for imagenet to run barlow twins
+# Ffcv dataloaders for imagenet to run barlow twinsr
 import torch
 import torchvision
 from typing import List
@@ -447,18 +447,23 @@ def get_ssltrain_imagenet_pytorch_dataloaders(
     }
 
     loaders = {}
-    if extra_augmentations:
-        transform = MultiViewTransform()
-    else:
-        transform = Transform()
 
     for name in ['train']:
-        dataset = torchvision.datasets.ImageFolder(paths[name], transform)
+        dataset = torchvision.datasets.ImageFolder(paths[name], Transform())
         loader = torch.utils.data.DataLoader(
             dataset, batch_size=batch_size, num_workers=num_workers,
-            pin_memory=True, shuffle=True, drop_last=True
+            pin_memory=False, shuffle=True, drop_last=True
         )
         loaders[name] = loader
+
+    if extra_augmentations:
+        for name in ['train_extra']:
+            dataset = torchvision.datasets.ImageFolder(paths['train'], MultiViewTransform())
+            loader = torch.utils.data.DataLoader(
+                dataset, batch_size=batch_size, num_workers=num_workers,
+                pin_memory=False, shuffle=True, drop_last=True
+            )
+            loaders[name] = loader
 
     return loaders
 
@@ -472,6 +477,9 @@ def get_ssleval_imagenet_pytorch_dataloaders(
     }
     
     if ood_eval:
+        train_dir = "/".join(data_dir.split("/")[:-1]) # removing noise_type
+        train_dir = "-".join(train_dir.split("-")[:-1]) # removing "-c" suffix from dataset
+        paths['train'] = train_dir + '/train'
         assert label_noise == 0, "Error: choose either ood_eval=True or label_noise > 0"
         paths.update(
             { f'test-{l}': data_dir + f'/{l}' for l in range(1, 6) }
@@ -479,12 +487,12 @@ def get_ssleval_imagenet_pytorch_dataloaders(
     else:
         paths['test'] = data_dir + '/val'
     
-    dset_cls = torchvision.datasets.ImageFolder
-    if label_noise > 0:
-        dset_cls = with_indices(dset_cls)
-
     loaders = {}
     for name in paths.keys():
+        dset_cls = torchvision.datasets.ImageFolder
+        if name == 'train' and label_noise > 0:
+            dset_cls = with_indices(dset_cls)
+
         dataset = dset_cls(paths[name], EvalTransform())
         if name == 'train' and label_noise > 0:
             assert not ood_eval, "Error: choose either ood_eval=True or label_noise > 0"
@@ -497,6 +505,7 @@ def get_ssleval_imagenet_pytorch_dataloaders(
                 targets=targets,
                 label_noise=label_noise
             )
+            dataset.samples = [ (s[0], t) for (s, t) in zip(dataset.samples, new_targets) ]
             try:
                 _ = dataset.targets
                 dataset.targets = new_targets
@@ -507,7 +516,7 @@ def get_ssleval_imagenet_pytorch_dataloaders(
         
         loader = torch.utils.data.DataLoader(
             dataset, batch_size=batch_size, num_workers=num_workers,
-            pin_memory=True, shuffle=False, drop_last=True
+            pin_memory=False, shuffle=False, drop_last=True
         )
         loaders[name] = loader
 
@@ -540,7 +549,8 @@ def get_ssltrain_imagenet_pytorch_dataloaders_distributed(
 class Transform:
     def __init__(self):
         self.transform = transforms.Compose([
-            transforms.RandomResizedCrop(224, interpolation=Image.BICUBIC),
+            #transforms.RandomResizedCrop(224, interpolation=Image.BICUBIC),
+            transforms.RandomResizedCrop(96, interpolation=Image.BICUBIC),
             transforms.RandomHorizontalFlip(p=0.5),
             transforms.RandomApply(
                 [transforms.ColorJitter(brightness=0.4, contrast=0.4,
@@ -555,7 +565,8 @@ class Transform:
                                 std=[0.229, 0.224, 0.225])
         ])
         self.transform_prime = transforms.Compose([
-            transforms.RandomResizedCrop(224, interpolation=Image.BICUBIC),
+            #transforms.RandomResizedCrop(224, interpolation=Image.BICUBIC),
+            transforms.RandomResizedCrop(96, interpolation=Image.BICUBIC),
             transforms.RandomHorizontalFlip(p=0.5),
             transforms.RandomApply(
                 [transforms.ColorJitter(brightness=0.4, contrast=0.4,
@@ -580,7 +591,7 @@ class MultiViewTransform(Transform):
         super(MultiViewTransform, self).__init__()
         self.n_augs = n_augs
         
-    def __call__(self, x)
+    def __call__(self, x):
         y1 = self.transform(x)
         output = (y1, ) + tuple(self.transform_prime(x) for _ in range(self.n_augs -1))
         return output 
@@ -588,6 +599,7 @@ class MultiViewTransform(Transform):
 class EvalTransform:
     def __init__(self):
         self.transform = transforms.Compose([
+            transforms.Resize((96, 96), interpolation=Image.BICUBIC),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                 std=[0.229, 0.224, 0.225])

fastssl/utils/label_correction.py
@@ -97,6 +97,7 @@ def with_indices(datasetclass):
         try:
             ground_truth = self._targets_orig[index]
         except AttributeError:
+            print("Warning: no ground truth found")
             ground_truth = target
         
         return data, target, ground_truth, index
@@ -113,7 +114,8 @@ def corrupt_labels(targets, label_noise, seed=1234):
     from numpy.random import default_rng
     rng = default_rng(seed)
 
-    if label_noise <= 0 return targets
+    if label_noise <= 0:
+        return targets
 
     if label_noise > 1:
         label_noise = float(label_noise) / 100.
@@ -124,7 +126,7 @@ def corrupt_labels(targets, label_noise, seed=1234):
         targets_tensor = torch.clone(targets)
 
     targets_shape = targets_tensor.shape
-    corrupt_idx = int(np.ceil(targets_tensor.shape[0] * noise_percentage))
+    corrupt_idx = int(np.ceil(targets_tensor.shape[0] * label_noise))
     num_classes = targets_tensor.unique().shape[0]
     if corrupt_idx == 0:
         return targets

fastssl/utils/powerlaw.py
@@ -76,6 +76,8 @@ def generate_activations_prelayer(net,layer,data_loader,use_cuda=False,dim_thres
     net.eval()
     for i, inp in enumerate(tqdm(data_loader)):
         (images, labels) = (inp[0], inp[1]) # discarding any extra item from the batch
+        if isinstance(images, (tuple, list)):
+            images = images[0]
         if use_cuda:
             images = images.cuda()
         with torch.no_grad():
@@ -90,7 +92,7 @@ def generate_activations_prelayer_torch(net,layer,data_loader,use_cuda=False,dim
     pool_size = 1
     activations = []
     def hook_fn(m,i,o):
-        activations.append(i[0])
+        activations.append(i[0].cpu().numpy())
     handle = layer.register_forward_hook(hook_fn)
 
     if use_cuda:

scripts/train_model_widthVary.py
@@ -218,7 +218,7 @@ def build_dataloaders(
                  batch_size,
                  num_workers,
                  extra_augmentations=extra_augmentations,
-            )             
+            )
         elif algorithm == "linear":
             default_linear_bsz = 512
 #            return imagenet_classifier_ffcv(
@@ -806,7 +806,7 @@ def precache_outputs(model, loaders, args, eval_args):
         # data = data.cuda(non_blocking=True)
         with autocast():
             with torch.no_grad():
-                out_augs = [model(x) for x in inp]
+                out_augs = [model(x.cuda()) for x in inp]
                 # mean of features across different augmentations of each image
                 out = torch.mean(torch.stack(out_augs), dim=0)
                 # out = model(data)
@@ -818,6 +818,7 @@ def precache_outputs(model, loaders, args, eval_args):
     trainset_outputs = torch.cat(trainset_outputs)
     trainset_labels = torch.cat(trainset_labels)
     if args.label_noise > 0:
+        breakpoint()
         trainset_ground_truths = torch.cat(trainset_ground_truths)
         trainset_sample_ids = torch.cat(trainset_sample_ids)
         total_corr = noise_ratio / num_samples * 100
@@ -836,7 +837,7 @@ def precache_outputs(model, loaders, args, eval_args):
         # data = data.cuda(non_blocking=True)
         with autocast():
             with torch.no_grad():
-                out_augs = [model(x) for x in inp]
+                out_augs = [model(x.cuda()) for x in inp]
                 # mean of features across different augmentations of each image
                 out = torch.mean(torch.stack(out_augs), dim=0)
                 # out = model(data)
@@ -913,13 +914,15 @@ def train(model, loaders, optimizer, loss_fn, args, eval_args, use_wandb=False, 
 
     if args.algorithm == "linear":
         if args.use_autocast:
-            activations = powerlaw.generate_activations_prelayer_torch(
+            #activations = powerlaw.generate_activations_prelayer_torch(
+            activations = powerlaw.generate_activations_prelayer(
                 net=model,
                 layer=model.fc,
                 data_loader=loaders["train"],
                 use_cuda=True,
             )
-            activations_eigen = powerlaw.get_eigenspectrum_torch(activations)
+            #activations_eigen = powerlaw.get_eigenspectrum_torch(activations)
+            activations_eigen = powerlaw.get_eigenspectrum(activations)
             with autocast():
                 try:
                     tmin, tmax = 3, min(50, activations.shape[1])
@@ -1003,13 +1006,15 @@ def train(model, loaders, optimizer, loss_fn, args, eval_args, use_wandb=False, 
         if epoch == 1:
             if args.track_alpha:
                 # compute alpha before training starts!
-                activations = powerlaw.generate_activations_prelayer_torch(
+                #activations = powerlaw.generate_activations_prelayer_torch(
+                activations = powerlaw.generate_activations_prelayer(
                     net=model,
                     layer=model.backbone.proj,
                     data_loader=loaders["train"],
                     use_cuda=True,
                 )
-                activations_eigen = powerlaw.get_eigenspectrum_torch(activations)
+                #activations_eigen = powerlaw.get_eigenspectrum_torch(activations)
+                activations_eigen = powerlaw.get_eigenspectrum(activations)
                 tmin, tmax = 3, min(100, activations.shape[1])
                 alpha, ypred, R2, R2_100 = powerlaw.stringer_get_powerlaw(
                     activations_eigen, trange=np.arange(tmin, tmax)
@@ -1117,7 +1122,7 @@ def train(model, loaders, optimizer, loss_fn, args, eval_args, use_wandb=False, 
                 ckpt_path = gen_ckpt_path(args, eval_args, epoch=epoch)
                 torch.save(state, ckpt_path)
 
-        elif epoch % args.log_interval == 0:
+        elif epoch % args.log_interval == 0 or epoch == args.epochs:
             ckpt_path = gen_ckpt_path(args, eval_args, epoch=epoch, suffix='pt')
             state = dict(
                 epoch=epoch + 1,
@@ -1131,13 +1136,15 @@ def train(model, loaders, optimizer, loss_fn, args, eval_args, use_wandb=False, 
             torch.save(state, ckpt_path)
             if args.track_alpha:
                 # compute alpha at intermediate training steps
-                activations = powerlaw.generate_activations_prelayer_torch(
+                #activations = powerlaw.generate_activations_prelayer_torch(
+                activations = powerlaw.generate_activations_prelayer(
                     net=model,
                     layer=model.backbone.proj,
                     data_loader=loaders["train"],
                     use_cuda=True,
                 )
-                activations_eigen = powerlaw.get_eigenspectrum_torch(activations)
+                #activations_eigen = powerlaw.get_eigenspectrum_torch(activations)
+                activations_eigen = powerlaw.get_eigenspectrum(activations)
                 tmin, tmax = 3, min(100, activations.shape[1])
                 alpha, ypred, R2, R2_100 = powerlaw.stringer_get_powerlaw(
                     activations_eigen, trange=np.arange(tmin, tmax)